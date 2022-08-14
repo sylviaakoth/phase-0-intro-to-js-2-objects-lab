8250478dc8a82aa07e4c562ecc9d31341d88c88d
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-08-14 12:05:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway',
+}
+
+let copyOf = {...employee}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return copyOf
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    employee.streetAddress = '12 Broadway'
+    return employee
+}
+
+let newEmployee = {...employee}
+
+function deleteFromEmployeeByKey(employee, key) {
+    delete newEmployee ["name"]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee ["name"]
+    return employee
+}
